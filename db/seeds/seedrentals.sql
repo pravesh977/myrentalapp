f70073752e45b871121a7ea53b19c9c84054d9bb
pravesh <pravesh_shrestha87@yahoo.com>
2017-08-30 01:54:42 -0400

    rentals seeds complete

---
+++ db/seeds/seedrentals.sql
@@ -0,0 +1,33 @@
+INSERT INTO rentaltable (title, description, bedrooms, bathrooms, city, state_id, zipcode, pets, parking, heating, cooling, availablefrom, price, wifi) VALUES
+    (
+        'Beautiful apartment at Elmhurst, Queens',
+        'This apartment is located at Elmhurst which is perfect for you if you love chinese food',
+        2,
+        1,
+        'Elmhurst',
+        32,
+        11373,
+        'YES',
+        'NO',
+        'YES',
+        'YES',
+        2017-10-12,
+        3200,
+        'YES'
+    ),
+    (
+        'Wonderful duplex at Beverly Hills',
+        'This 5000 sq ft. apartment is everything you wish for if you like to live large',
+        4,
+        3,
+        'Beverly Hills',
+        5,
+        90210,
+        'YES',
+        'YES',
+        'NO',
+        'YES',
+        2017-09-10,
+        10000,
+        'YES'
+    );
