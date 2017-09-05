7e18dcce25f3824c1187da8445f79779f7223da0
pravesh <pravesh_shrestha87@yahoo.com>
2017-09-05 03:26:43 -0400

    index complete

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.listen(port);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
--- db/config.js
+++ db/config.js
@@ -5,13 +5,17 @@ if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
     db = pgp({
         database: 'nepaliroomsdb',
         port: 5432,
-        host: 'localhost'
+        host: 'localhost',
+        user: 'postgres',
+        password: 'postgres'
     });
 } else if (process.env.NODE_ENV === 'production') {
     db = pgp({
         database: 'nepaliroomsdb_production',
         port: 5432,
-        host: 'localhost'
+        host: 'localhost',
+         user: 'postgres',
+        password: 'postgres'
     });
 }
 module.exports = db;
