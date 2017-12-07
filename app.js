adfb33c7c5ae819eb8e11706278c13d941e95174
Pravesh Shrestha <praveshshrestha@Praveshs-MacBook-Pro.local>
2017-12-07 10:56:28 -0500

    amskl

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use('/static', express.static(path.join(__dirname, 'client/reactrentalapp/build')));
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/reactrentalapp/build')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -36,7 +36,7 @@ app.use('/emailroute', emailroute);
 
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/reactrentalapp/build', 'index.html'));
 });
 
 console.log(`nepali roommate server running on port ${port}`);
